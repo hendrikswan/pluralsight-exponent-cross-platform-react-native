251b375c4d9b5272cc4af1c3b7312cb34713837b
Hendrik Swanepoel <hendrik.swanepoel@gmail.com>
2015-12-15 22:18:19 +0200

    got the app working in android

--- index.ios.js
+++ index.ios.js
@@ -2,8 +2,7 @@
  * Sample React Native App
  * https://github.com/facebook/react-native
  */
-'use strict';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import CrossTodo from './main';
 
 AppRegistry.registerComponent('CopyFromExponent', () => CrossTodo);
